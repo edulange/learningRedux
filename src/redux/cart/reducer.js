61553d1528fae5088c8024eaaeee0ae3f6b5bdff
Eduardo Lange <eduardoandrelange@gmail.com>
2024-04-30 18:58:11 -0300

    ok

--- src/redux/cart/reducer.js
+++ src/redux/cart/reducer.js
@@ -8,10 +8,27 @@ const initialState = {
 const cartReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case CartActionTypes.ADD_PRODUCT:
-			return {
-				...initialState,
-				products: [...initialState.products, action.payload],
+			//verificar se o produto já está no carrinho
+			const productIsAlreadyInCart = state.products.some((product) => product.id === action.payload.id)
+
+			//se estiver no carrinho aumentar a quantidade em 1
+			if (productIsAlreadyInCart) {
+				return {
+					...state,
+					products: state.products.map((product) =>
+						product.id === action.payload.id ? { ...product, quantity: product.quantity + 1 } : product
+					),
+				}
 			}
+		//se ele não estiver, adiciona-lo
+		return {
+			...state,
+			products: [...state.products, { ...action.payload, quantity: 1}]
+		}
+		/* aqui era no começo return {
+				...state,
+				products: [...state.products, action.payload],
+			} */
 		default:
 			return state
 	}
